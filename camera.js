5074072596fa009503cc7aa82b00d9a390e52ce6
mspcity <codepark2019@gmail.com>
2021-05-30 18:51:14 -0400

    dom added

--- camera.js
+++ camera.js
@@ -1,2 +1,7 @@
 // dom
 const recordButton = document.querySelector(".record-button")
+const stopButton = document.querySelector(".stop-button")
+const playButton = document.querySelector(".play-button")
+const downloadButton = document.querySelector(".download-button")
+const previewPlayer = document.querySelector("#preview")
+const recordingPlayer = document.querySelector("#recording")
